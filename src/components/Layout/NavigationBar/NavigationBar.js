9c511830fbaf1c60b40ff4dc06d80f5d7b0d341f
niteesh <niteeshb7@gmail.com>
2019-12-07 02:44:16 +0000

    Added Navigation bar
    Signed-off-by: niteesh <niteeshb7@gmail.com>

--- src/components/Layout/NavigationBar/NavigationBar.js
+++ src/components/Layout/NavigationBar/NavigationBar.js
@@ -1,3 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {useHistory} from "react-router-dom";
+import LoginContext from "../../../context/LoginContext";
+import RouterConfig from '../router-config';
 
-const NavigationBar = () => <nav></nav>;
+const NavigationBar = () => {
+    const {signInInfo} = useContext(LoginContext);
+    const history = useHistory();
+
+    return <nav>
+        <ul>
+            {RouterConfig && Object.values(RouterConfig).filter(route =>
+                [RouterConfig.ORDERS.path, RouterConfig.PROFILE.path].includes(route && route.path) ? signInInfo : true
+            ).map(route => route &&
+                <li key={route.path} onClick={() => history.push(route.path)}>{route.displayText}</li>)}
+        </ul>
+    </nav>;
+
+};
+
+export default NavigationBar;
--- src/components/Layout/hoc/WithLayout/WithLayout.js
+++ src/components/Layout/hoc/WithLayout/WithLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "../../Header/Header";
+import NavigationBar from "../../NavigationBar/NavigationBar";
 
-const WithLayout = ({children}) => <><Header/>{children}</>;
+const WithLayout = ({children}) => <><Header/><NavigationBar/>{children}</>;
 
 export default WithLayout;
