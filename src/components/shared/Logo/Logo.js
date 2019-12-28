b3ff6837f55a54e3a704b2ecfb1232638ab2537a
niteesh <niteeshb7@gmail.com>
2019-12-28 15:33:19 +0000

    Converted logo text to svg
    Signed-off-by: niteesh <niteeshb7@gmail.com>

--- src/components/shared/Logo/Logo.js
+++ src/components/shared/Logo/Logo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import LogoSvg from './logo.svg';
+import LogoText from './Onilne-kitchen-text-logo.svg';
 
-const Logo = props => <div {...props} ><img src={LogoSvg} alt={'logo img'}/><span>Online Kitchen</span></div>;
+const Logo = props => <div {...props} ><img src={LogoSvg} alt={'logo img'}/>
+    <div><img alt={'logo text'} src={LogoText}/></div>
+</div>;
 
 export default Logo;
