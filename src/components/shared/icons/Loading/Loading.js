33c2afeee4190558c3fef27e33193cb45339d4bf
niteesh <niteeshb7@gmail.com>
2020-01-26 23:02:44 +0000

    Upgraded loader formula based on 2*Math.PI.radius
    Signed-off-by: niteesh <niteeshb7@gmail.com>

--- src/components/shared/icons/Loading/Loading.js
+++ src/components/shared/icons/Loading/Loading.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './Loading.module.scss';
 
 const Loading = ({size, color}) => <svg className={styles.LoadingIcon} height={size} width={size} style={{color}}>
-    <circle cx={size / 2} cy={size / 2} r={.4 * size} strokeWidth={.073 * size} fill="none"/>
+    <circle cx={size / 2} cy={size / 2} r={.4 * size} strokeWidth={.073 * size} strokeDasharray={.8 * size * Math.PI}
+            fill="none"/>
 </svg>;
 
 Loading.propTypes = {
